77a89475b2b32898e9a62b0b4a583ef263389a93
Your Name <you@example.com>
2014-11-27 10:04:02 +0000

    Simplify the main loop, drop all sleeps

--- test_preview_no_tunneling.c
+++ test_preview_no_tunneling.c
@@ -16,7 +16,10 @@
 #include "m_components.h"
 #include "bcm_host.h"
 
-void consume_queue_on_connection(MMAL_CONNECTION_T *conn, MMAL_PORT_T *port);
+#define MMAL_PORT_TYPE_INPUT  2
+#define MMAL_PORT_TYPE_OUTPUT 3
+
+void consume_queue_on_connection(MMAL_PORT_T *port, MMAL_QUEUE_T *queue);
 
 void connection_callback(MMAL_CONNECTION_T *conn)
 {
@@ -25,38 +28,28 @@ void connection_callback(MMAL_CONNECTION_T *conn)
 
 static void camera_control_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
 {
- 	//fprintf(stderr,"[%ld] Camera Callback called\n", pthread_self());
+ //	fprintf(stderr,"[%ld] Camera Callback called\n", pthread_self());
    	mmal_buffer_header_release(buffer);
 }
 
 static void preview_control_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
 {
- 	//fprintf(stderr,"[%ld] Preview Control Callback called on port %p\n", pthread_self(), port);
-	vcos_sleep(40);
+ //	fprintf(stderr,"[%ld] Preview Control Callback called on port %p\n", pthread_self(), port);
    	mmal_buffer_header_release(buffer);
 }
 
-void consume_queue_on_connection(MMAL_CONNECTION_T *conn, MMAL_PORT_T *port)
+void consume_queue_on_connection(MMAL_PORT_T *port, MMAL_QUEUE_T *queue)
 {
 	MMAL_BUFFER_HEADER_T *buffer;
-	if ((buffer = mmal_queue_get(conn->pool->queue)) != NULL)
-	{    
-		while(buffer)
-		{
+	
+	while ((buffer = mmal_queue_get(queue)) != NULL)
+	{
 			if (mmal_port_send_buffer(port, buffer) != MMAL_SUCCESS)
 			{
-				mmal_queue_put_back(conn->pool->queue, buffer);
+				mmal_queue_put_back(queue, buffer);
 				break;
 			}
-			else if (port->type == 2) // type 2 -> IN
-			{
-				buffer = mmal_queue_get(conn->queue);
-			} else if (port->type == 3) // type 3 -> OUT
-			{
-				buffer = mmal_queue_get(conn->pool->queue);
-			}
-		}
-	}    
+	}
 }
 
 int main(void)
@@ -70,42 +63,50 @@ int main(void)
 	// bcm should be initialized before any GPU call is made
 	bcm_host_init();
 
-	vcos_assert(( init_state() == 0) && "Checking setting initial state failed");
+	vcos_assert(( init_state() == 0) 
+		&& "Checking setting initial state failed");
 
-	vcos_assert((mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera) == MMAL_SUCCESS) && "Failed creating camera component");
+	vcos_assert((mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera) == MMAL_SUCCESS) 
+		&& "Failed creating camera component");
 
-	vcos_assert((mmal_port_enable(camera->control, camera_control_callback) == MMAL_SUCCESS) && "Failed enabling camera control port");
+	vcos_assert((mmal_port_enable(camera->control, camera_control_callback) == MMAL_SUCCESS) 
+		&& "Failed enabling camera control port");
 
 	set_port_default_format(camera->output[0]->format);
-	vcos_assert((mmal_port_format_commit(camera->output[0]) == MMAL_SUCCESS) && "Failed setting preview port format");
+	vcos_assert((mmal_port_format_commit(camera->output[0]) == MMAL_SUCCESS) 
+		&& "Failed setting preview port format");
 
-	vcos_assert((mmal_component_enable(camera) == MMAL_SUCCESS) && "Failed to enable camera component");
+	vcos_assert((mmal_component_enable(camera) == MMAL_SUCCESS) 
+		&& "Failed to enable camera component");
 
-	vcos_assert((mmal_component_create(MMAL_COMPONENT_DEFAULT_VIDEO_RENDERER, &preview) == MMAL_SUCCESS) && "Failed creating preview component");
-	vcos_assert((mmal_port_enable(preview->control, preview_control_callback) == MMAL_SUCCESS) && "Failed enabling preview control port");
-	vcos_assert(( set_preview_component_defaults(preview) == MMAL_SUCCESS) && "Failed setting preview components default values");
+	vcos_assert((mmal_component_create(MMAL_COMPONENT_DEFAULT_VIDEO_RENDERER, &preview) == MMAL_SUCCESS) 
+		&& "Failed creating preview component");
+	
+	vcos_assert((mmal_port_enable(preview->control, preview_control_callback) == MMAL_SUCCESS) 
+		&& "Failed enabling preview control port");
 
-	vcos_assert((mmal_component_enable(preview) == MMAL_SUCCESS) && "Failed to enable camera component");
+	vcos_assert(( set_preview_component_defaults(preview) == MMAL_SUCCESS) 
+		&& "Failed setting preview components default values");
+
+	vcos_assert((mmal_component_enable(preview) == MMAL_SUCCESS) 
+		&& "Failed to enable camera component");
 
 	vcos_assert((mmal_connection_create(&preview_conn, camera->output[0], preview->input[0], 
 		MMAL_CONNECTION_FLAG_ALLOCATION_ON_INPUT) == MMAL_SUCCESS) 
 		&& "Failed stablishing connection");
 
-
-	vcos_assert((mmal_connection_enable(preview_conn) == MMAL_SUCCESS) && "Failed enabling connection");
+	vcos_assert((mmal_connection_enable(preview_conn) == MMAL_SUCCESS) 
+		&& "Failed enabling connection");
 
 	preview_conn->callback = connection_callback;
 
+	/* Main loop
+	 */
 	while (1)
 	{
-		static unsigned capturing = 1;
-		
-		capturing=!capturing;
-		mmal_port_parameter_set_boolean(camera->output[1], MMAL_PARAMETER_CAPTURE, capturing);
-
-		vcos_sleep(40);
-		consume_queue_on_connection(preview_conn, preview_conn->in);
-		consume_queue_on_connection(preview_conn, preview_conn->out);
+		mmal_port_parameter_set_boolean(camera->output[1], MMAL_PARAMETER_CAPTURE, 1);
+		consume_queue_on_connection(preview_conn->out, preview_conn->pool->queue);
+		consume_queue_on_connection(preview_conn->in, preview_conn->queue);
 	}
 
 	vcos_assert(( finalize_state() == 0) && "Failed freeing memory nicely" );
